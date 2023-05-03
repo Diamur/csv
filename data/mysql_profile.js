bc54405b1ec16cec3e53fe719a7e36c31f6089b3
Diamur <neelovdima@gmail.com>
2023-05-03 15:37:46 +0300

    mysql_wget

---
+++ data/mysql_profile.js
@@ -0,0 +1,21 @@
+
+function data(name) {
+switch(name){
+   case "vangre_002" : return {
+    host     : 'vangre.beget.tech',
+    user     : 'vangre_002',
+    password : '&Hlz6m6m',
+    database : 'vangre_002'
+   };
+   case "vkcable_db7" : return {
+    host     : 'vkcable.beget.tech',
+    user     : 'vkcable_db7',
+    password : 'W4wxb9WqZ5XD',
+    database : 'vkcable_db7'
+   };
+    default : return {};
+}
+
+}
+  
+module.exports = { data };
